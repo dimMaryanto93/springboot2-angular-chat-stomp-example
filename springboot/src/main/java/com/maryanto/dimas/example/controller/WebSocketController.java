df16d596404951d67958c5f81c82bdda4f074a27
dimasm93 <software.dimas_m@icloud.com>
2019-08-07 17:36:12 +0700

    websocket with springboot

--- springboot/src/main/java/com/maryanto/dimas/example/controller/WebSocketController.java
+++ springboot/src/main/java/com/maryanto/dimas/example/controller/WebSocketController.java
@@ -1,4 +1,24 @@
 package com.maryanto.dimas.example.controller;
 
+import com.maryanto.dimas.example.entity.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import java.time.LocalDateTime;
+
+@Controller
 public class WebSocketController {
+
+    @MessageMapping("/chat.start")
+    @SendTo("/topic/greetings")
+    public String halo(@Payload Message value) {
+        return String.format(
+                "hello from %s at %s",
+                HtmlUtils.htmlEscape(value.getName()),
+                LocalDateTime.now().toString()
+        );
+    }
 }
--- springboot/src/main/java/com/maryanto/dimas/example/entity/Message.java
+++ springboot/src/main/java/com/maryanto/dimas/example/entity/Message.java
@@ -1,4 +1,13 @@
 package com.maryanto.dimas.example.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
+
+    private String name;
 }
