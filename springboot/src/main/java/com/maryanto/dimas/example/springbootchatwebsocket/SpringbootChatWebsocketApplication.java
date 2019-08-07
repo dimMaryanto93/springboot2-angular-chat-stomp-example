df7216c40383c19a2a3336beae80dd902e5eac10
dimasm93 <software.dimas_m@icloud.com>
2019-08-07 10:36:34 +0700

    init springboot project

---
+++ springboot/src/main/java/com/maryanto/dimas/example/springbootchatwebsocket/SpringbootChatWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example.springbootchatwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChatWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootChatWebsocketApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/maryanto/dimas/example/springbootchatwebsocket/SpringbootChatWebsocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maryanto.dimas.example.springbootchatwebsocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootChatWebsocketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
