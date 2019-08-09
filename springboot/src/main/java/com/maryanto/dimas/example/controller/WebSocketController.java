f5ef75ab555d3e729a08994262591adb17f3cd8d
dimasm93 <software.dimas_m@icloud.com>
2019-08-09 10:39:42 +0700

    update configuration web socket

--- springboot/src/main/java/com/maryanto/dimas/example/controller/WebSocketController.java
+++ springboot/src/main/java/com/maryanto/dimas/example/controller/WebSocketController.java
@@ -1,37 +1,21 @@
 package com.maryanto.dimas.example.controller;
 
 import com.maryanto.dimas.example.entity.Message;
-import com.maryanto.dimas.example.entity.OutputMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Slf4j
 @Controller
 public class WebSocketController {
 
-    @Autowired
-    private SimpMessagingTemplate template;
-
     @MessageMapping("/private")
     @SendTo("/chat/send")
-    public OutputMessage halo(
-            @Payload Message message,
-            SimpMessageHeaderAccessor headerAccessor) {
+    public Message halo(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
         log.info("received: {}", message);
-        OutputMessage output = new OutputMessage(
-                message.getMessage(),
-                message.getFrom(),
-                Timestamp.valueOf(LocalDateTime.now()));
-        headerAccessor.getSessionAttributes().put("username", message.getFrom());
-        return output;
+        return message;
     }
 }
--- springboot/src/main/java/com/maryanto/dimas/example/entity/Message.java
+++ springboot/src/main/java/com/maryanto/dimas/example/entity/Message.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Message {
 
     private String message;
-    private String from;
+    private String fromId;
+    private String toId;
+    private Timestamp dateSent;
+    private Timestamp dateSeen;
 }
